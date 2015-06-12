e8bf200cd4ceb1107fa171a4e18e159b96b63065
andip71 <andreasp@gmx.de>
2015-06-12 19:35:35 +0200

    Revert "Boeffla ramfs: Default settings only when no startup config + remove async startup (starter file) again"
    This reverts commit 3ccef3c19836a48c920a36f2c4f60fd856af5a0f.

--- ramdisk_boeffla/fs/sbin/boeffla-init.sh
+++ ramdisk_boeffla/fs/sbin/boeffla-init.sh
@@ -90,6 +90,21 @@
 	/sbin/busybox rm -f $BUSYBOX_ENABLER
 	/sbin/busybox rm -f $FRANDOM_ENABLER
 	
+# Apply Boeffla-Kernel default settings 1
+
+	# Ext4 tweaks default to on
+	/sbin/busybox sync
+	mount -o remount,commit=4,noatime $CACHE_DEVICE /cache
+	/sbin/busybox sync
+	mount -o remount,commit=4,noatime $DATA_DEVICE /data
+	/sbin/busybox sync
+
+	# dynamic fsync to on
+	echo 1 > /sys/kernel/dyn_fsync/Dyn_fsync_active
+	/sbin/busybox sync
+
+	echo $(date) Boeffla-Kernel default settings 1 applied >> $BOEFFLA_LOGFILE
+
 # Execute early startconfig placed by Boeffla-Config V2 app, if there is one
 	if [ -f $BOEFFLA_STARTCONFIG_EARLY ]; then
 		echo $(date) "Early startup configuration found"  >> $BOEFFLA_LOGFILE
@@ -122,6 +137,14 @@
 	echo $(date) Rom boot trigger detected, waiting a few more seconds... >> $BOEFFLA_LOGFILE
 	/sbin/busybox sleep 20
 
+# Apply Boeffla-Kernel default settings 2
+
+	# Sdcard buffer tweaks default to 1024 kb
+	echo 1024 > /sys/block/mmcblk0/bdi/read_ahead_kb
+	echo 1024 > /sys/block/mmcblk1/bdi/read_ahead_kb
+
+	echo $(date) Boeffla-Kernel default settings 2 applied >> $BOEFFLA_LOGFILE
+
 # Interaction with Boeffla-Config app V2
 	# save original stock values for selected parameters
 	cat /sys/devices/system/cpu/cpu0/cpufreq/UV_mV_table > /dev/bk_orig_cpu_voltage
@@ -141,25 +164,11 @@
 		. $BOEFFLA_STARTCONFIG
 		echo $(date) Startup configuration applied  >> $BOEFFLA_LOGFILE
 	else
-		# Ext4 tweaks default to on
-		/sbin/busybox sync
-		mount -o remount,commit=4,noatime $CACHE_DEVICE /cache
-		/sbin/busybox sync
-		mount -o remount,commit=4,noatime $DATA_DEVICE /data
-		/sbin/busybox sync
-
-		# dynamic fsync to on
-		echo 1 > /sys/kernel/dyn_fsync/Dyn_fsync_active
-		/sbin/busybox sync
-
-		# Sdcard buffer tweaks default to 1024 kb
-		echo 1024 > /sys/block/mmcblk0/bdi/read_ahead_kb
-		echo 1024 > /sys/block/mmcblk1/bdi/read_ahead_kb
-
+		echo $(date) "No startup configuration found"  >> $BOEFFLA_LOGFILE
+		
 		# If not, apply default Boeffla-Kernel vnswap of 1300 MB
 		/res/bc/bccontroller.sh apply_zram 1 1 1363148800
-
-		echo $(date) "No Startup configuration found, apply Boeffla-Kernel default settings" >> $BOEFFLA_LOGFILE
+		echo $(date) Boeffla default vnswap activated >> $BOEFFLA_LOGFILE
 	fi
 	
 # Turn off debugging for certain modules
