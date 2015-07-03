bfaa814781c95dfc8fe163eaf8bf8fae22f45e05
andip71 <andreasp@gmx.de>
2015-07-03 16:47:13 +0200

    Boeffla ramfs: Revert back to zzmoove 1.0-beta6a

--- ramdisk_boeffla/fs/res/bc/bccontroller.sh
+++ ramdisk_boeffla/fs/res/bc/bccontroller.sh
@@ -35,7 +35,7 @@ RECOVERY_DEVICE="/dev/block/platform/msm_sdcc.1/by-name/recovery"
 # *******************
 
 if [ "lov_gov_profiles" == "$1" ]; then
-	echo "interactive - battery;interactive - battery extreme;interactive - performance;zzmoove - optimal;zzmoove - battery;zzmoove - battery plus;zzmoove - battery yank;zzmoove - battery extreme yank;zzmoove - performance;zzmoove - insane;zzmoove - moderate;zzmoove - game;zzmoove - relax"
+	echo "interactive - battery;interactive - battery extreme;interactive - performance;zzmoove - optimal;zzmoove - battery;zzmoove - battery plus;zzmoove - battery yank;zzmoove - battery extreme yank;zzmoove - performance;zzmoove - insane;zzmoove - moderate;zzmoove - game"
 	exit 0
 fi
 
@@ -532,10 +532,6 @@ if [ "apply_cpu_hotplug_profile" == "$1" ]; then
 		if [ `busybox cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor | busybox grep zzmoove` ]; then
 			echo "0" > /sys/devices/system/cpu/cpufreq/zzmoove/disable_hotplug
 			echo "0" > /sys/devices/system/cpu/cpufreq/zzmoove/disable_hotplug_sleep
-
-			if [ `busybox cat /sys/devices/system/cpu/cpufreq/zzmoove/profile_number | busybox grep 11` ]; then
-				echo "2" > /sys/devices/system/cpu/cpufreq/zzmoove/hotplug_min_limit
-			fi
 		else
 			echo "0" >/sys/devices/system/cpu/cpu0/online_control
 			echo "0" >/sys/devices/system/cpu/cpu1/online_control
@@ -977,13 +973,6 @@ if [ "apply_governor_profile" == "$1" ]; then
 
 	if [ "zzmoove - game" == "$2" ]; then
 		echo "10" > /sys/devices/system/cpu/cpufreq/zzmoove/profile_number
-
-		busybox sleep 0.5s
-		busybox sync
-	fi
-
-	if [ "zzmoove - relax" == "$2" ]; then
-		echo "11" > /sys/devices/system/cpu/cpufreq/zzmoove/profile_number
 
 		busybox sleep 0.5s
 		busybox sync
